98c39ae0ad11be2f50a7e53d4afd70403198821a
cummiew <cummiew@gmail.com>
2017-02-01 17:34:32 -0500

    started new files, added test cases, successfully checked that numbers were equal to 10

---
+++ isbn.rb
@@ -0,0 +1,16 @@
+def isbn_10_chkr(isbn_num)
+		if ((isbn_num.length < 10) || (isbn_num.length > 10))
+
+			return false
+		else
+			return true
+
+		end
+end
+
+def isbn_dash_deleter(isbn_num)
+		new_num = isbn_num.delete"-"
+		return new_num 
+
+end
+
---
+++ test_isbn.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require_relative "isbn.rb"
+
+	class Testisbn < Minitest::Test
+
+		def test_length_is_equal_to_10
+			assert_equal(true,isbn_10_chkr("123456789x"))
+
+		end
+
+		def test_length_is_less_than_10
+			assert_equal(false,isbn_10_chkr("12345"))
+		
+		end
+
+		def test_for_deletion_of_dashes
+			assert_equal("12345",isbn_dash_deleter("1-2-3-4-5-"))
+		
+		end
+
+	end
