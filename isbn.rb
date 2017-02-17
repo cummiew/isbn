d56004c410a3898d91f806e91e43e0e1bf8d001d
cummiew <cummiew@gmail.com>
2017-02-16 20:37:04 -0500

    Added 13 digit function. Also added valid and invalid functions.

--- isbn.rb
+++ isbn.rb
@@ -13,60 +13,37 @@ def isbn_dash_deleter(isbn_num)
 			return new_num 
 
 end
-		
-# def isbn_dash_space_deletion(isbn_num)
-# 			new_num = isbn_num.delete" "  # new_num becomes isbn_num.delete
-# 			return new_num
+def	check_the_sum_greater_than_ten(refreshed)
 
-# end		
+	original=refreshed
+	sum = 0; index = 0; refreshed = refreshed.chop; alternating_multiplier = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3]
+	refreshed.each_char {|number| sum += (number.to_i * alternating_multiplier[index]); index += 1}
+	final=((10 - (sum % 10)) % 10).to_s
 
-# def isbn_sum_10_chkr(isbn_num)
-# 		 	isbn_num_array = Array.new
-# 			return new_num
+	if refreshed+final==original
+		valid_isbn_thirteen(original)
+	else
+		invalid_isbn(refreshed)
+	end
 
-# end
+end
 
+def valid_isbn_ten(refreshed)		
 
-def isbn_sum_13_chkr(isbn_num)
-			isbn_num_array = Array.new
+		puts "\nCongratulations!\nYour ISBN #{refreshed.join("")} is Valid!\nHave a nice day:-)\n\n"
+		true
+end
 
-		
-	 # sum_mult == 11 % isbn_num[-1].to_i  Can not do the num 11 with the modulo, % placed after.
-	 
-	 end
+def valid_isbn_thirteen(original)
 
-# end
-	
+		puts "\nCongratulations!\nYour ISBN #{original} is Valid!\nHave a nice day:-)\n\n"
+		true
+end
 
-# 		 if	sum_mult % 11 == isbn_num[-1].to_i
-# 				return true
+def invalid_isbn(refreshed)
 
-# 		elsif  (sum_mult % 11) == 10 && isbn_num[-1].downcase == "x"
-# 				return true
+		puts "\nSorry 'bout your luck!\nYou got hold of a counterfeit ISBN!\nBetter luck next time...\n\n"
 
-# 		else
-# 				return false
+		false
 
-# 		end
-
-
-# 			return new_num
-
-# end
-
-# 		# test_pass_13_and_10_together
-# 		# assert_equal(true,valid_isbn?("978-0-13-149505-0"))lhjhhf
-
-# 		def valid_isbn?(num_str)
-# 			no_spaces_no_dashes = deletion_of_dashes_spaces(num_str)
-
-# 		if no_spaces_no_dashes.length == 10 && !(check_for_symbols(no_spaces_no_dashes)) && check_x(no_spaces_no_dashes)
-# 				(isbn_sum_10_chkr(no_spaces_no_dashes))
-
-# 		elsif no_spaces_no_dashes.length == 13 && !(check_for_symbols(no_spaces_no_dashes)) && check_x(no_spaces_no_dashes)
-  				
-#   				return true
-#   		else
-# 				return false
-# 	end
-# end
+end
